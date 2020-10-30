9e74032bc905221fb02dd2a6dd4a814d1a304080
Azael Rivera <azaelkiller@gmail.com>
2020-10-29 19:56:19 -0700

    lexicalAnalyzer updated

--- main.c
+++ main.c
@@ -1,55 +1,211 @@
 #include <stdio.h>
+#include <conio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
-void leer(char *buffer, char *main_code,int space)
+enum TipoToken
 {
-    char lector;
-    FILE *archivo = fopen(main_code, "r");
-    if (archivo == NULL)
+    PalRes = 1,
+    Id = 2,
+    Num = 3,
+    Sim = 4
+};
+
+struct Token
+{
+    char Nombre[80];
+    enum TipoToken Tipo;
+    char Lexema[80];
+    int Valor;
+    int NoLin;
+    int NoCol;
+};
+
+struct nodo
+{
+    struct Token info;
+    struct nodo *izq;
+    struct nodo *der;
+};
+
+/* Global */
+struct nodo *raiz = NULL;
+struct nodo *actual = NULL;
+char bufferLectura[100] = "";
+int estadoAsignacion = 0;
+char tipoAsignacion = 0;
+
+int asignarPalRes(char *palabra)
+{
+    if (palabra == "INCIO" 
+        || palabra == "FIN" 
+        || palabra == "POR" 
+        || palabra == "SI" 
+        || palabra == "MOSTRAR" 
+        || palabra == "REGRESA" 
+        || palabra == "ENTONCES" 
+        || palabra == "ROMPER" 
+        || palabra == "CONTRA" 
+        || palabra == "VAR")
     {
-        printf("Error: Could not open file\n");
+        tipoAsignacion = 1;
+        return 1;
     }
     else
     {
-        fread(buffer, sizeof(char), space, archivo);
+        return 0;
     }
-    fclose(archivo);
 }
 
-void leerToken(char caracter){
-    switch(caracter){
-        
-    }
-}
-/*
-void PToken(char *caracter)
+int asignarIdentificador(char caracter)
 {
-    int i=0;
-    int y=0;
-    char *prueba=malloc(strlen(caracter)+1);
-    while(caracter[i] != '\0'){
-        if(caracter[i] == ' ' || caracter[i] == ';' || caracter[i] == '\0'){
-            printf("%s\n", prueba);
-            //printf("%s\n",prueba);
-            memset(prueba, '\0', strlen(prueba));
-            y=0;
-        }
-        else{
-            prueba[y]=caracter[i];
-            y++;
-        }
-        i++;
+    if(isalpha(caracter) && estadoAsignacion==0){
+        tipoAsignacion = 2;
+        estadoAsignacion = 1;
+        bufferLectura[strlen(bufferLectura)] = caracter;
+        return 0;
+    }
+    else if(isalnum(caracter) && estadoAsignacion==1){
+        bufferLectura[strlen(bufferLectura)] = caracter;
+        return 0;
+    }  
+    else{
+        return 1;
     }
 }
-*/
+
+int asignarConstante(char caracter)
+{
+    if(isdigit(caracter)){
+        estadoAsignacion = 1;
+        tipoAsignacion = 3;
+        bufferLectura[strlen(bufferLectura)] = caracter;
+        return 0;
+    }
+    else if(isdigit(caracter) && estadoAsignacion==1){
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
+int asignarSimbolo(char caracter)
+{
+    if (caracter == ';' 
+        || caracter == '{'
+        || caracter == '='
+        || caracter == '}'
+        || caracter == '<' 
+        || caracter == '>' 
+        || caracter == '&' 
+        || caracter == '|'
+        || caracter == '('
+        || caracter == ')'
+        || caracter == '/'
+        || caracter == '+'
+        || caracter == '-'
+        || caracter == '.'
+        || caracter == '"'
+        || caracter == '!')
+    {
+        bufferLectura[strlen(bufferLectura)] = caracter;
+        tipoAsignacion = 4;
+        return 1;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+void insertar(struct Token token)
+{
+    struct nodo *nuevo;
+    nuevo = malloc(sizeof(struct nodo));
+
+    nuevo->info = token;
+    nuevo->izq = NULL;
+    nuevo->der = NULL;
+
+    if (raiz == NULL)
+    {
+        raiz = nuevo;
+        actual = nuevo;
+    }
+    else
+    {
+        nuevo->izq = actual;
+        actual->der = nuevo;
+        actual = nuevo;
+    }
+}
+
+void imprimirLista(struct nodo *reco)
+{
+    if (reco != NULL)
+    {
+        printf("Nombre: %s\n", reco->info.Nombre);
+        printf("Tipo: %d\n", reco->info.Tipo);
+        printf("Lexema: %s\n", reco->info.Lexema);
+        printf("Valor: %d\n", reco->info.Valor);
+        printf("-------\n");
+        imprimirLista(reco->der);
+    }
+}
+
+void leer(char *path){
+    FILE *archivo;
+    char caracter;
+    char fila = 1;
+    char columna = 1;
+    int estadoLectura = 0;
+    archivo = fopen(path, "r");
+    if(archivo == NULL){
+        printf("Fallo en la lectura del archivo %s", path);
+    }else{
+       while ((caracter=fgetc(archivo)) != EOF)
+       {
+           if(tipoAsignacion == 0 || tipoAsignacion == 2)
+           {
+                estadoLectura = asignarIdentificador(caracter);
+           }else if(tipoAsignacion == 0 || tipoAsignacion == 3)
+           {
+                estadoLectura = asignarConstante(caracter);
+           }else if(tipoAsignacion == 0 || tipoAsignacion == 4)
+           {
+                estadoLectura = asignarSimbolo(caracter);
+           }
+           if(estadoLectura == 1){
+               if(tipoAsignacion == 2){
+                   asignarPalRes(bufferLectura);
+               }
+                struct Token token;
+                strcpy(token.Nombre, "ParentesisDer");
+                token.Tipo = tipoAsignacion;
+                if(atoi(bufferLectura)){
+                    token.Valor = atoi(bufferLectura);
+                }else{
+                    token.Valor = 0; 
+                }
+                strcpy(token.Lexema, bufferLectura);
+                free(bufferLectura);
+                insertar(token);
+                tipoAsignacion = 0;
+                estadoAsignacion = 0;
+                estadoLectura = 0;
+           }
+           
+       }
+    }
+}
+
 int main()
 {
-    char buffer[256];
-    char main_code[] = "test.ck";
-    leer(buffer, main_code,256);
-    printf("%s", buffer);
-    //PToken(buffer);
+     printf("=====LISTA DE TOKENS=====\n");
+    leer("test.ck");
+   
+    imprimirLista(raiz);
+    getch();
     return 0;
 }
